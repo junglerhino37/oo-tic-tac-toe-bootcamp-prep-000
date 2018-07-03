e5b0987ec4e4622a16e9423a6cb1ea248d1f81bf
Christopher Rouxel <chris.rouxel88@gmail.com>
2018-07-03 22:26:29 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -49,13 +49,13 @@ def valid_move?(index)
   index.between?(0,8) && !position_taken?(index)
 end
 
-def turn(board)
+def turn
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
-  if valid_move?(board, index)
-    move(board, index,current_player(board))
-    display_board(board)
+  if valid_move?(index)
+    move(index,current_player)
+    display_board
   else
     turn(board)
   end
